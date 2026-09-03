7d77149afc871a9ecb7fb253178e9bbb29f2abdb
Rushikesh Joshi <official.smellsfromheaven@gmail.com>
2026-09-03 20:15:43 +0530

    fix price display decimals

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,7 +3,7 @@ export function formatPrice(price: number): string {
     style: "currency",
     currency: "INR",
     minimumFractionDigits: 0,
-    maximumFractionDigits: 0,
+    maximumFractionDigits: 2,
   }).format(price);
 }
 
